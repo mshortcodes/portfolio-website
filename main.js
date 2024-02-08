be899793a384d2c3710211f538825afd40f5cd14
Michael Short <mshort0403@gmail.com>
2024-02-07 20:13:10 -0800

    Add scroll padding

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import './style.css';
+
+// add extra padding when scrolling to prevent header from blocking content
+
+const header = document.querySelector('header');
+const headerHeight = document.querySelector('header').clientHeight;
+
+document.documentElement.style.setProperty(
+	'--scroll-padding',
+	`${headerHeight}px`,
+);
