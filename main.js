ce5c5cdfcf9c2cd10cb9cd66add666f0e9aeef09
Michael Short <mshort0403@gmail.com>
2024-02-07 23:23:29 -0800

    Add mobile navigation

--- main.js
+++ main.js
@@ -2,10 +2,37 @@ import './style.css';
 
 // add extra padding when scrolling to prevent header from blocking content
 
-const header = document.querySelector('header');
-const headerHeight = document.querySelector('header').clientHeight;
+const headerHeight = document.querySelector('header').offsetHeight;
 
 document.documentElement.style.setProperty(
 	'--scroll-padding',
 	`${headerHeight}px`,
 );
+
+// mobile nav
+
+const headerBtn = document.getElementById('headerBtn');
+const mobileNav = document.getElementById('mobileNav');
+const mobileLinks = document.querySelectorAll('.mobile_link');
+
+let isMobileNavOpen = false;
+
+headerBtn.addEventListener('click', () => {
+	isMobileNavOpen = !isMobileNavOpen;
+	if (isMobileNavOpen) {
+		mobileNav.style.display = 'flex';
+		document.body.style.overflowY = 'hidden';
+		window.scrollTo(0, 0);
+	} else {
+		mobileNav.style.display = 'none';
+		document.body.style.overflowY = 'auto';
+	}
+});
+
+mobileLinks.forEach((link) => {
+	link.addEventListener('click', () => {
+		isMobileNavOpen = false;
+		mobileNav.style.display = 'none';
+		document.body.style.overflowY = 'auto';
+	});
+});
